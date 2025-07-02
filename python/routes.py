f27dd19cb26cc6961dd2e97846282b3b372b0f26
fatma18765 <mariemedial2016@gmail.com>
2025-07-02 23:14:24 +0000

    Nouvellespages

--- python/routes.py
+++ python/routes.py
@@ -7,33 +7,10 @@ from python.Fonctions import inscriptionetu,inscriptionprof,connexionprof,connex
 def index():
     return render_template('accueil.html')
 
-@app.route('/connectionprofpage')
-def Connexionprof():
-    return render_template('connexion_prof.html')
 
-@app.route('/connectionetudiantpage')
-def Connexionetu():
-    return render_template('connexion_etudiant.html')
-
-@app.route('/inscriptionetudiantcode', methods=['POST'])
-def Inscription():
-    return inscriptionetu()
-
-@app.route('/connexionprofcode', methods=['POST'])
-def ConnexionProf():
-    return connexionprof()
-
-@app.route('/connexionetudiantcode', methods=['POST'])
-def ConnexionEtudiant():
-    return connexionetudiant()
-
-@app.route('/inscriptionprofcode', methods=['POST'])
-def Inscriptionprof():
-    return inscriptionprof()
-
-@app.route('/inscriptionprofpage')
-def Inscriptionprofpage():
-    return render_template('inscription_prof.html')
+@app.route('/chefdedepartement')
+def pagechefdepartement():
+    return render_template('chefdedepartement.html')
 
 @app.route('/connexionprof')
 def connexionprof_route():
@@ -43,124 +20,29 @@ def inscriptionprof_route():
     
     return render_template('inscriptionprof.html')
 
+@app.route('/consulterlesdemandes')
+def consulterpage_route():
+    
+    return render_template('consulter.html')
 
-@app.route('/inscriptionetudiantpage')
-def Inscriptionetupage():
-    return render_template('inscription_etudiant.html')
+@app.route('/soumettredemande')
+def soumettre_demande():
+    
+    return render_template('soumettredemande.html')
 
-@app.route('/pageEtudiant')
-def ConnexionP():
-    return render_template('pageEtudiant.html')
-
-@app.route('/info_devcode',methods=['POST'])
-def Infodev():
-    return infodev()
-
-@app.route('/InscriptionProf')
-def InscriptionP():
-    return render_template('InscriptionP.html')
-
-@app.route('/Profil')
-def Profil():
-    return render_template('Profil.html')
-
-@app.route('/ajouterun_devoir', methods=['POST'])
-def Ajouterundevoir():
-    return ajouter_devoir()
-
-@app.route('/info_copiecode',methods=['POST'])
-def copiecode():
-    return infocopiecode()
-
-@app.route('/notercopie',methods=['POST'])
-def Notecopie():
-    return notercopie()
-
-@app.route('/updatenote',methods=['POST'])
-def Updatenote():
-    return updatenote()
-
-@app.route('/ajouterun_devoir', methods=['POST'])
-def Ajoutdevoir_route():
-    return ajouter_devoir()
-
-@app.route('/soumettre',methods=['POST'])
-def Soumettre():
-    return soumettrefichier()
-
-@app.route('/timelineP')
-def timelineprof_route():
-    return timeline_prof()
-
-@app.route('/examen')
-def examen_route():
-    return afficher_examens()
-
-@app.route('/accueilp')
-def accueilp_route():
-    return countcopiesnonnotees()
-
-@app.route('/ajoutdevoirpage')
-def AjoutAdmin_route():
-    return render_template('Ajoutdevoir.html',sess_username=session['username'],sess_id=session['id'])
-
-@app.route('/copies')
-def copiesexamen():
-    return copieparexam()
-
-@app.route('/copie' ,methods=['POST'])
-def copie_examen():
-    return timeline_prof()
-
-@app.route('/notep')
-def note_prof():
-    return afficher_note()
-
-@app.route('/statistiquesp')
-def statistiques():
-    return statistiquesp()
-
-@app.route("/accueiletudiant")
-def accueiletudiant():
-    return statistiques_etudiant()
-
-@app.route("/examen")
-def examen():
-    return afficher_examens()
-
-@app.route('/devoirs')
-def devoirs_page():
-    return afficher_devoirs()
-
-@app.route('/notes')
-def notes():
-    return afficher_notes()
-
-@app.route('/notifications')
-def notifications():
-    return afficher_notifications()
-
-@app.route('/deconnection')
-def deconnecter():
-    return deconnection()
-
-@app.route('/trier_classe',methods=['POST'])
-def triparclasse():
-    return trier_classe()
-
-@app.route('/trier_date',methods=['POST'])
-def tripardate():
-    return trier_date()
-
-@app.route('/noteexam',methods=['POST'])
-def voirnotes():
-   return voirlesnotes()
-@app.route('/chatbot', methods=['POST'])
-def ollama():
-    return chatbot()
-@app.route('/generernote',methods=['POST'])
-def genererfichiernote():
-    return generernote()
-
+@app.route('/synthese')
+def synthese_route():
+    
+ return render_template('synthese.html')
+@app.route('/transmettredemande')
+def transmettre_route():
+    return render_template('transmettre.html')
+    
+@app.route('/deconnexion')
+def deconnexion_route():
+    
+    return render_template('accueil.html')       
+    
+    return render_template('synthese.html')
 if __name__ == '__main__':
     app.run(debug=True)
